a3051c970477731872b4e5145cdc74ce224e4fa2
arboledaLeon <arboledaleon46@gmail.com>
2025-08-30 22:58:00 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
+    #return render(request, 'home.html')
+    return render(request, 'home.html', {'name':'León Arboleda'})
+
+
+def about(request):
+    return HttpResponse("This is the About Page")
+
